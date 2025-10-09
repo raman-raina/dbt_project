c298671722117dcbae316e464ef43604191fc728
Raman Raina <raina20raman03@gmail.com>
2025-10-09 14:40:19 +0530

    my_first_model.sql

--- my_first_model.sql
+++ my_first_model.sql
@@ -1,3 +1,6 @@
-SELECT *
-FROM data
-WHERE Profit > 1000
+SELECT 
+    o."Customer_ID", o."Customer_Name", c."Age", o."City", o."State", o."Region", o."Profit"
+FROM orders AS o
+INNER JOIN customers AS c
+    ON o."Customer_ID" = c."Customer_ID"
+WHERE o."Profit" > 1000
